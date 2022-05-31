21085b74063c83ae4ad825b74e6507ffd6444a3f
Nedudigital <segunchinedu2000@gmail.com>
2022-05-31 20:23:06 +0100

    new methods

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+/**
+ * check_cycle - check if a linked list contains a cycle
+ * @list: linked list to check
+ * 
+ * Return: 1 if cycle is present, 0 if cycle is absent
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *slow = list;
+    listint_t *fast = list;
+
+    if (!list)
+    return (0);
+
+    while (slow && fast && fast ->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        return (1);
+    }
+}
