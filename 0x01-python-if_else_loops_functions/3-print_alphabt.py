93710c0c2b3a8bef06c5ba6e6076b656adcd5ecc
Nedudigital <segunchinedu2000@gmail.com>
2022-06-11 21:49:05 +0100

    Arsene Lupin

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Tolulope Fakunle
-for letter in range(97, 123):
+# Author - Chinedu Ndubisi
+for letter in range(100, 126):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
